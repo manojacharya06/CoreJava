f91495b476c0a42b07c12dcf854631265fc88769
manojacharya06 <115134486+manojacharya06@users.noreply.github.com>
2022-11-05 09:48:29 +0530

    Add files via upload

---
+++ Pattern6.java
@@ -0,0 +1,26 @@
+package dataTypes;
+
+public class Pattern6 {
+
+	public static void main(String[] args) {
+		int count=0;
+		for(int i=1;i<=4;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{	
+				count=count +1;
+				System.out.print(count + " ");
+			}
+			System.out.println();						
+		}
+
+	}
+
+}
+
+/*Output:
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+*/
